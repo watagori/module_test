688beb103351095ddbc9eac789f6076a900083f2
watagori <cottongorillla@gmail.com>
2022-01-13 08:55:58 +0000

    web3

--- run.py
+++ run.py
@@ -1,17 +1,3 @@
-# 自作にしたパッケージを呼び出す
-from view import viewParent
-from view import viewChild
-from model import modelParent
-from model import modelChild
-
-p_model = modelParent()
-p_model.myName()
-
-c_model = modelChild()
-c_model.myName()
-
-p_view = viewParent()
-p_view.myName()
-
-c_view = viewChild()
-c_view.myName()
+from web3 import Web3, EthereumTesterProvider
+w3 = Web3(EthereumTesterProvider())
+print(w3.isConnected())
--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+
+def load_requires_from_file(filepath):
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
+
 setup(
     name='mySample',  # パッケージ名
     version="0.0.1",
@@ -10,5 +16,6 @@ setup(
     classifiers=[
         "Development Status :: 1 - Planning"
     ],
-    packages=['view', 'model']  # パッケージのサブフォルダー
+    packages=['view', 'model'],  # パッケージのサブフォルダー
+    install_requires=['web3']
 )
